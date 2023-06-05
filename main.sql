645a097ded5a63f128173eff4a44180115f80f0c
Salvador HM <salvadorhm@gmail.com>
2023-06-05 07:51:27 -0600

    CREATE Alias

--- main.sql
+++ main.sql
@@ -11,15 +11,15 @@ PRAGMA foreign_keys = ON;
 
 .import --skip 1 csv/clientes.csv clientes
 .import --skip 1 csv/productos.csv productos
-.import --skip 1 csv/ventas.csv ventas
+.import --skip 1 csv/ventas.csv ventas 
 .import --skip 1 csv/detalle_ventas.csv detalle_ventas
 
 .mode box
 
-SELECT ventas.id_venta,clientes.nombre, ventas.fecha, productos.producto, detalle_ventas.cantidad_producto, detalle_ventas.precio_unitario, detalle_ventas.total_x_producto
-FROM detalle_ventas
-INNER JOIN ventas ON detalle_ventas.id_venta = ventas.id_venta
-INNER JOIN clientes ON ventas.id_cliente = clientes.id_cliente
-INNER JOIN productos ON detalle_ventas.id_producto = productos.id_producto
-ORDER BY ventas.id_venta;
+SELECT v.id_venta,c.nombre, v.fecha, p.producto, dv.cantidad_producto, dv.precio_unitario, dv.total_x_producto
+FROM detalle_ventas as dv
+INNER JOIN ventas as v ON dv.id_venta = v.id_venta
+INNER JOIN clientes as c ON v.id_cliente = c.id_cliente
+INNER JOIN productos as p ON dv.id_producto = p.id_producto
+ORDER BY v.id_venta;
 
