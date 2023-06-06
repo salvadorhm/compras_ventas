83c0873e2af44c22c8b28772dfe00dcdefe0bde9
Salvador HM <salvadorhm@gmail.com>
2023-06-06 10:49:12 -0600

    COMPARED virtual y stored columnas generadas

---
+++ pruebas/stored.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS temporal(
+    id_temporal integer PRIMARY KEY AUTOINCREMENT,
+    valor1 float,
+    valor2 float,
+    total float as (valor1 * valor2) STORED
+);
+
+.import --csv datos.csv t
+
+INSERT INTO temporal(valor1, valor2)
+SELECT valor1,valor2
+FROM t;
+
+DROP TABLE t;
